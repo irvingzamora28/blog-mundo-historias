6fdfb4c10da122d69ccae2e215e8d005dd826f49
Irving Zamora <irvingzamora28@gmail.com>
2023-04-28 00:24:17 -0600

    Add Logo Mundo Historias

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,7 +1,7 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
+  title: 'Historias y Experiencias',
   author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  headerTitle: 'Mundo Historias',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
